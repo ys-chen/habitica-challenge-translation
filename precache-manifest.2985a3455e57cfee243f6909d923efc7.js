8b966496496f8865ea72c0f03288660980dd8bd1
Nikki Chen <yangsyuan8012@gmail.com>
2020-08-20 17:35:58 +0800

    Updates

---
+++ precache-manifest.2985a3455e57cfee243f6909d923efc7.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c671c370d094ef871d273c305550e088",
+    "url": "/habitica-challenge-translation/index.html"
+  },
+  {
+    "revision": "0794232b3cf334758c1f",
+    "url": "/habitica-challenge-translation/static/css/2.947cfe3f.chunk.css"
+  },
+  {
+    "revision": "c3b5fc27c8d01bc72f67",
+    "url": "/habitica-challenge-translation/static/css/main.c88df015.chunk.css"
+  },
+  {
+    "revision": "0794232b3cf334758c1f",
+    "url": "/habitica-challenge-translation/static/js/2.c7d32fa1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/habitica-challenge-translation/static/js/2.c7d32fa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3b5fc27c8d01bc72f67",
+    "url": "/habitica-challenge-translation/static/js/main.abfba5db.chunk.js"
+  },
+  {
+    "revision": "f62fe2654d10d0242cb3",
+    "url": "/habitica-challenge-translation/static/js/runtime-main.0d2ef56e.js"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/habitica-challenge-translation/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/habitica-challenge-translation/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/habitica-challenge-translation/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/habitica-challenge-translation/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/habitica-challenge-translation/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/habitica-challenge-translation/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/habitica-challenge-translation/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/habitica-challenge-translation/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/habitica-challenge-translation/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/habitica-challenge-translation/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/habitica-challenge-translation/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/habitica-challenge-translation/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/habitica-challenge-translation/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/habitica-challenge-translation/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/habitica-challenge-translation/static/media/fa-solid-900.ee09ad75.woff"
+  }
+]);
