a23672e0fa8c3374b1cee6546db36175da1fb4d4
Nikki Chen <yangsyuan8012@gmail.com>
2020-08-24 17:52:05 +0800

    fix: RWD component order

--- src/routes/ChallengeDetail.jsx
+++ src/routes/ChallengeDetail.jsx
@@ -32,6 +32,18 @@ function ChallengeDetail({ challengeId }) {
   const [t] = useTranslation();
   const [state] = useState(meta.find(({ officialId }) => (officialId === challengeId)));
   const { shortName, tasksOrder } = state;
+  const desktopClass = 'd-none d-lg-block';
+  const mobileClass = 'd-block d-lg-none';
+
+  const GoToButton = ({ className }) => (
+    <a
+      className={`btn btn-success w-100 ${className}`}
+      href="https://habitica.com/challenges/8d6e280a-4dc8-4957-b2b4-feb07ce9ff6b"
+      target="_blank"
+      rel="noopener noreferrer">
+      {t('toChallengePage')}
+    </a>
+  );
 
   const TaskColumn = ({ title, taskType }) =>
     tasksOrder.some((type) => type === taskType) ? (
@@ -56,23 +68,19 @@ function ChallengeDetail({ challengeId }) {
 
   return state && (
     <div className="row">
-      <div className="col-12 col-lg-8 p-4">
-        <h1 className="text-primary">{t(`${shortName}name`)}</h1>
+      <div className="col-12 col-lg-8 order-2 order-lg-1 p-4">
+        <h1 className={`text-primary ${desktopClass}`}>{t(`${shortName}name`)}</h1>
         <div className="row">
           <TaskColumn title={t('habits')} taskType="habit" />
           <TaskColumn title={t('dailies')} taskType="daily" />
           <TaskColumn title={t('todos')} taskType="todo" />
           <TaskColumn title={t('rewards')} taskType="rewards" />
         </div>
+        <GoToButton className={mobileClass} />
       </div>
-      <div className="col-12 col-lg-4 bg-light pt-4">
-        <a
-          className="btn btn-success w-100"
-          href="https://habitica.com/challenges/8d6e280a-4dc8-4957-b2b4-feb07ce9ff6b"
-          target="_blank"
-          rel="noopener noreferrer">
-          {t('toChallengePage')}
-        </a>
+      <div className="col-12 col-lg-4 order-1 order-lg-2 bg-light pt-4">
+        <h1 className={`text-primary ${mobileClass}`}>{t(`${shortName}name`)}</h1>
+        <GoToButton className={desktopClass} />
         <hr />
         <Intro collapsable title={t('summary')}>
           <p>{t(`${shortName}summary`)}</p>
